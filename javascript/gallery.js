e4a05202b3dd4845f7fde0a08b3960c39b3de170
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-04-08 00:35:43 +0200

    made js shorter
    shorthanded some js codes on gallery

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,41 +1,18 @@
-const headshotButton = document.getElementById('headshot-btn');
-const tinderButton = document.getElementById('tinder-btn');
-const weddingButton = document.getElementById('wedding-btn');
-const boudoirButton = document.getElementById('boudoir-btn');
+const buttons = [
+    { button: document.getElementById('headshot-btn'), headerText: "Headshots", imagesId: 'headshot-images' },
+    { button: document.getElementById('tinder-btn'), headerText: "Tinder", imagesId: 'tinder-images' },
+    { button: document.getElementById('wedding-btn'), headerText: "Wedding", imagesId: 'wedding-images' },
+    { button: document.getElementById('boudoir-btn'), headerText: "Boudoir", imagesId: 'boudoir-images' }
+
+];
 const header = document.getElementById("header");
-const headshotImages = document.getElementById('headshot-images');
-const tinderImages = document.getElementById('tinder-images');
-const weddingImages = document.getElementById('wedding-images');
-const boudoirImages = document.getElementById('boudoir-images');
 
-headshotButton.addEventListener("click", function() {
-    header.innerHTML = "Headshots";
-    headshotImages.style.display = 'grid';
-    tinderImages.style.display = 'none';
-    weddingImages.style.display = 'none';
-    boudoirImages.style.display = 'none';
-})
-
-tinderButton.addEventListener("click", function() {
-    header.innerHTML = "Tinder";
-    headshotImages.style.display = 'none';
-    tinderImages.style.display = 'grid'
-    weddingImages.style.display = 'none';
-    boudoirImages.style.display = 'none'
-})
-
-weddingButton.addEventListener("click", function() {
-    header.innerHTML = "Tinder";
-    headshotImages.style.display = 'none';
-    tinderImages.style.display = 'none'
-    weddingImages.style.display = 'grid';
-    boudoirImages.style.display = 'none'
-})
-
-boudoirButton.addEventListener("click", function() {
-    header.innerHTML = "Tinder";
-    headshotImages.style.display = 'none';
-    tinderImages.style.display = 'none'
-    weddingImages.style.display = 'none';
-    boudoirImages.style.display = 'grid'
-})
+buttons.forEach(({ button, headerText, imagesId }) => {
+    button.addEventListener("click", function() {
+        header.innerHTML = headerText;
+        buttons.forEach(({ imagesId }) => {
+            document.getElementById(imagesId).style.display = 'none';
+        });
+        document.getElementById(imagesId).style.display = 'grid';
+    });
+});
