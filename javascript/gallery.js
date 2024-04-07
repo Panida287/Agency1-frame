eb066b865ab608825669415514116fee148cff64
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-04-07 23:50:25 +0200

    played around with js

---
+++ javascript/gallery.js
@@ -0,0 +1,7 @@
+const headshotButton = document.getElementById('headshot-btn');
+const tinderButton = document.getElementById('tinder-btn');
+const weddingButton = document.getElementById('wedding-btn');
+const boudoirButton = document.getElementById('boudoir-btn');
+const gallery = document.getElementById('gallery');
+const header = document.getElementById("header");
+
